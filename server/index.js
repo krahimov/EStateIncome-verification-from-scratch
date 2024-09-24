6bc9efdae36af6a0ee961601527dc2cc5b9e9e16
krahimov <keryabakugan@gmail.com>
2024-09-24 13:06:28 -0500

    create a simple server to make Plaid calls

---
+++ server/index.js
@@ -0,0 +1,24 @@
+// Plaid backend API integration with Node.js
+app.post('/api/create_link_token', async function (request, response) {
+    // Get the client_user_id by searching for the current user
+    const user = await User.find(...);
+    const clientUserId = user.id;
+    const request = {
+      user: {
+        // This should correspond to a unique id for the current user.
+        client_user_id: clientUserId,
+      },
+      client_name: 'Plaid Test App',
+      products: ['auth'],
+      language: 'en',
+      webhook: 'https://webhook.example.com',
+      redirect_uri: 'https://domainname.com/oauth-page.html',
+      country_codes: ['US'],
+    };
+    try {
+      const createTokenResponse = await client.linkTokenCreate(request);
+      response.json(createTokenResponse.data);
+    } catch (error) {
+      // handle error
+    }
+  });
